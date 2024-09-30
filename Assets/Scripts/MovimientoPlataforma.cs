f9edf58adab30fb73c949ac3a415a2ce7cb47a72
anyuegonzalez <anyuee07@gmail.com>
2024-09-30 17:09:41 +0200

    ngfn

---
+++ Assets/Scripts/MovimientoPlataforma.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPlataforma : MonoBehaviour
+{
+    [SerializeField] float velocidad = 30;
+    [SerializeField] Vector3 direccion;
+    [SerializeField] int tiempo;
+    [SerializeField] float timer = 0;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.Translate(direccion = new Vector3(12, 0, 0) * velocidad * Time.deltaTime);
+        if (timer >= 0)
+        {
+            transform.Translate(direccion = new Vector3(12, 0, 0) * -1 * velocidad * Time.deltaTime);
+            timer += 1 * Time.deltaTime;
+        }
+          
+    }
+
+}
---
+++ Assets/Scripts/Rotacion.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 180f, 0f) * Time.deltaTime);
+    }
+}
