6999723b1f9fb92d59ead9f043c77ba6c4429c65
anyuegonzalez <anyuee07@gmail.com>
2024-10-07 16:16:58 +0200

    fewf

--- Assets/Scripts/Bola.cs
+++ Assets/Scripts/Bola.cs
@@ -6,9 +6,10 @@ public class Bola : MonoBehaviour
 {
 
     [SerializeField] Vector3 direccion = new Vector3(0,0,0);
-    [SerializeField] float fuerza = 12f;
-    [SerializeField] float fuerzaSalto, velocidad = 7f;  
+    [SerializeField] float fuerza = 12f, h, v;
+    [SerializeField] float fuerzaSalto = 15f, velocidad = 7f, fuerzaMovimiento =  15f;  
     private bool isGrounded = true;
+    
 
     Rigidbody rb;
 
@@ -20,28 +21,20 @@ public class Bola : MonoBehaviour
    
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        Vector3 moveDirection = new Vector3(h, 0, v);
-        transform.position += moveDirection * velocidad * 13 * Time.deltaTime;
-        Salto();
+         h = Input.GetAxisRaw("Horizontal");
+         v = Input.GetAxisRaw("Vertical");
 
+        Salto();
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(h, 0, v).normalized * fuerzaMovimiento, ForceMode.Force);
     }
     void Salto()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded )
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
-            isGrounded = false;
-        }
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true; 
         }
     }
 }
