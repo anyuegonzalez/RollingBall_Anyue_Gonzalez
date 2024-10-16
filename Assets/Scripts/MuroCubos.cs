31c393a8322fd7ace066eff40693c570814fa993
anyuegonzalez <anyuee07@gmail.com>
2024-10-16 19:37:56 +0200

    agfweg

--- Assets/Scripts/MuroCubos.cs
+++ Assets/Scripts/MuroCubos.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MuroCubos : MonoBehaviour
 {
+    [SerializeField] private Rigidbody[] rbs;
+
+    private float timer = 0f;
+    private bool iniciarCuenta = false;
  
     void Start()
     {
@@ -14,12 +18,28 @@ public class MuroCubos : MonoBehaviour
     void Update()
     {
         
+        if (iniciarCuenta)
+        {
+            timer += 1 * Time.unscaledDeltaTime;
+            if(timer >= 2)
+            {
+
+                Time.timeScale = 1f;
+                for (int i = 0; i < 3; i++)
+                {
+
+                }
+
+            }
+        }
+        
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             Time.timeScale = 0.3f;
+            iniciarCuenta = true;
         }
     }
 }
